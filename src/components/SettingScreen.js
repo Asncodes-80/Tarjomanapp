f3128d212cb9cc537ee4af7b56461ff5a291a3ea
alirezasoltanineshan <asn80.asn@hotmail.com>
2020-05-25 12:49:06 +0430

    create platform in setting screen
    nothing, i only added some platform version of my app

--- src/components/SettingScreen.js
+++ src/components/SettingScreen.js
@@ -1,50 +1,75 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Platform, Dimensions, Linking, ScrollView, I18nManager} from 'react-native';
-import { Ionicons, MaterialCommunityIcons  } from '@expo/vector-icons';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Platform,
+    Dimensions,
+    Linking,
+    ScrollView,
+    I18nManager
+} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
-const SettingScreen = ({navigation}) =>{
-  I18nManager.allowRTL(false);
-    return(
-        <>   
-          <View style={styles.mainContent}>
-            <TouchableOpacity onPress={()=>{ navigation.navigate('AppearanceScreen');}}>
-              <View style={styles.appearanceSection}>
-                  <Text style={{
-                    fontSize: 18, 
+const SettingScreen = ({navigation}) => {
+    I18nManager.allowRTL(false);
+    return (
+        <>
+            <View style={styles.mainContent}>
+                <TouchableOpacity onPress={() => {
+                    navigation.navigate('AppearanceScreen');
+                }}>
+                    <View style={styles.appearanceSection}>
+                        <Text style={{
+                            fontSize: 18,
+                            textAlign: 'right',
+                            marginRight: 20,
+                        }}>تغییر ظاهر کد</Text>
+                        <Ionicons name="ios-arrow-back" size={24} color="#CFCFD1" style={{marginLeft: 20}}/>
+                    </View>
+                </TouchableOpacity>
+                <Text style={{
                     textAlign: 'right',
                     marginRight: 20,
-                    }}>تغییر ظاهر کد</Text>
-                  <Ionicons name="ios-arrow-back" size={24} color="#CFCFD1" style={{marginLeft: 20}} />
-              </View>
-            </TouchableOpacity>
-              <Text style={{
-                textAlign: 'right', 
-                marginRight: 20, 
-                marginTop: 8,
-                fontWeight: '200',
-                color: '#75757A'}}>شما میتوانید استایل موردنیاز خود را به محیط کد دهید.</Text>
-               
-          </View>
-     
-
+                    marginTop: 8,
+                    fontWeight: '200',
+                    color: '#75757A'
+                }}>شما میتوانید استایل موردنیاز خود را به محیط کد دهید.</Text>
+            </View>
+            {/* Version and some info section*/}
+            <View style={styles.detailVer}>
+                <Text
+                    style={styles.detailVerText}>{Platform.OS === 'ios' ? "ترجمان نسخه آی او اس" : "ترجمان نسخه اندروید"}</Text>
+                <Text style={styles.detailVerText}>ترجمان ورژن 3.5</Text>
+            </View>
         </>
     )
 }
 const styles = StyleSheet.create({
-  mainContent:{
-    flexDirection: 'column',
-    marginTop: Platform.OS === 'ios'? 100: 50,
-    alignSelf: 'center',
-    alignItems: 'flex-end'
-  },
-  appearanceSection:{
-    flexDirection: 'row-reverse',
-    width: Dimensions.get('window').width-30,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 17,
-    alignItems: 'center',
-    height: 60,
-    justifyContent: 'space-between'
-  }
- })
+    mainContent: {
+        flexDirection: 'column',
+        marginTop: Platform.OS === 'ios' ? 100 : 50,
+        alignSelf: 'center',
+        alignItems: 'flex-end'
+    },
+    appearanceSection: {
+        flexDirection: 'row-reverse',
+        width: Dimensions.get('window').width - 30,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 17,
+        alignItems: 'center',
+        height: 60,
+        justifyContent: 'space-between'
+    },
+    detailVer: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginTop: 50
+    },
+    detailVerText: {
+        color: '#75757A',
+        marginBottom: 5
+    }
+})
 export default SettingScreen;
